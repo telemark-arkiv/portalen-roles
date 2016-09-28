e1ab2598d2a27c2e4f8d12749efc9ca236385f40
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-28 12:31:19 +0200

    Moves tests. Adds basic test

--- test/lib/generate-id-test.js
+++ test/lib/generate-id-test.js
@@ -1,8 +1,8 @@
 'use strict'
 
 var tap = require('tap')
-var generateId = require('../utils/generate-id')
-var testData = require('./data/test-data-generate-id.json')
+var generateId = require('../../utils/generate-id')
+var testData = require('./../data/test-data-generate-id.json')
 
 testData.forEach(function (item) {
   tap.equal(item.expected, generateId(item.input), 'It returns expected result from ' + item.input)
